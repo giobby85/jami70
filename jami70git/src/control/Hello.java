ba13d7fb632bab08d5b6a8b92b3c01f5a76a4354
giovanni <giovanni@DESKTOP-O4EOVCK>
2022-12-15 15:09:10 +0100

    aggiornamento del metodo get

--- jami70git/src/control/Hello.java
+++ jami70git/src/control/Hello.java
@@ -26,8 +26,10 @@ public class Hello extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String temp = request.getParameter("test");
+		if(temp.equals("pippo")) {
+			System.out.print("ciao");
+		}
 	}
 
 	/**
